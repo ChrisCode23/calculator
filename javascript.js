dd6112164dc6b15b8c6f32840e03fe3e16e84018
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-09-12 17:35:11 +0200

    Add operation variables
    A calculator operation consist of a number,
    an operator and another number (e.g  3 + 5).
    These are now enclosed within variables and will
    be useful when working on updating the display

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
+// Basic math operations
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+// Represent each part of a calculator operation, will be useful to update display
+const firstNum;
+const operator;
+const secondNum;
