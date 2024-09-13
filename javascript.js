d98abb78df66c69b3371b50e4bc90990c89c41ad
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-09-13 15:57:11 +0200

    Allow multiple digits and set operator type
    Fixed an issue where numbers were only formed
    of one digit. Now it's possible to add two numbers
    , which are stored in two variables, and when
    selecting an operator, an according function will
    execute based on the operator type. In order to
    do so, the basic math functions are now stored in
    an object variable "operations"

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 // Basic math operations
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const operations = {};
+
+operations.add = (a, b) => a + b;
+operations.subtract = (a, b) => a - b;
+operations.multiply = (a, b) => a * b;
+operations.divide = (a, b) => a / b;
+
 
 // Represent each part of a calculator operation, will be useful to update display
 let firstNum;
-let operator;
+let operatorType;
 let secondNum;
 
 const display = document.querySelector(".display");
@@ -15,32 +18,40 @@ const operators = document.querySelectorAll(".operator");
 const equal = document.querySelector(".equal");
 const clear = document.querySelector(".clear");
 
-// Stores digits and operators which get progressively added to the display
-let displayNums = [];
+//A display variable keeps track of the numbers input in display by user
+//User must be able to enter numbers with multiple digits e.g "23"
+//Default is 0, but we want to replace 0 with the number we input
+//User input first number which is stored in firstNum variable and displays it
+//Once user is done entering digits of numbers
+//User chooses an operator which adds the firstNum variable to the operation, which type is determined by the operator chosen
+//User enters the second number which is stored in the secondNum variable and displays it
+//By pressing equal, returns the result of the operation in the display
 
+    
     digits.forEach((digit) => {
         digit.addEventListener(("click"), () => {
-            displayNums.push(+digit.id);
-            display.textContent += `${displayNums[displayNums.length - 1]} `
+            display.textContent += `${digit.id}`
         })
     })
 
     operators.forEach((operator) => {
         operator.addEventListener(("click"), () => {
-            displayNums.push(operator.id);
-            display.textContent += `${displayNums[displayNums.length - 1]} `
+            firstNum = display.textContent;
+            display.textContent = "";
+            operatorType = operator.id;
         })
     })
     
     equal.addEventListener(("click"), () => {
-        firstNum = displayNums[0];
-        secondNum = displayNums[2];
-        operate(displayNums);
+        secondNum = display.textContent;
+        operate(firstNum, secondNum, operatorType);
     })
 
-    // Clearing empties display array, so that the result matches new operations
+    // Clears display and empty previous variables in order to avoid getting same result down the line
     clear.addEventListener(("click"), () => {
-        displayNums.splice(0, displayNums.length);
+        firstNum = null;
+        operatorType = null;
+        secondNum = null;
         display.textContent = "";
     })
 
@@ -48,36 +59,13 @@ let displayNums = [];
 
 
 // Checks the operator and based on it's type, executes a specific operation
-const operate = (displayNums) => {
+const operate = (firstNum, secondNum, operator) => {
     let result;
-    switch (displayNums[1]) {
-        case "+":
-            result = add(firstNum, secondNum);
-            display.textContent = `${result}`;
-            break;
-        case "-":
-            result = subtract(firstNum, secondNum);
-            display.textContent = `${result}`;
-            break;
-        case "*":
-            result = multiply(firstNum, secondNum);
-            display.textContent = `${result}`;
-            break;
-        case ":":
-            result = divide(firstNum, secondNum);
-            display.textContent = `${result}`;
-            break;
+    if (operations[operatorType]) {
+        result = operations[operatorType](+firstNum, +secondNum)
     }
+    display.textContent = result;
+    
 }
 
 
-//Store the digited content, both numbers and operators in an array
-//Push the digit in the array when clicking digit
-//Same goes for operators
-//When pressing equal button, run the operate function
-//Function adds first element and third element of the array
-//Every second element is considered an operator 7
-//That is going to decide exactly which of the operations from operate() will trigger
-//Operate() decides which operation to execute based on the value of the operator in the display
-/*e.g When pressing = button => displayNums[0] = 3, displayNums[2] = 5, both of these are stored in firstNum and secondNum variables
-and  displayNums[1] = +, so we'll execute the add() function */
